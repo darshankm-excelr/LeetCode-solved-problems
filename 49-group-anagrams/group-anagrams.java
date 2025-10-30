34f6d12b3c4603b775c1e93a19db6f9d4f73fa95
Darshan  K M <157715317+darshankm-excelr@users.noreply.github.com>
2025-10-30 10:43:39 +0530

    Time: 15 ms (28.01%) | Memory: 48 MB (57.31%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,43 @@
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+
+        Map<String, List<String>> frequencyStringMap = new HashMap<>();
+
+        for (String str : strs) {
+            String frequencyString = getFrequency(str);
+
+            if (frequencyStringMap.containsKey(frequencyString)) {
+                frequencyStringMap.get(frequencyString).add(str);
+            } else {
+                List<String> strList = new ArrayList<>();
+                strList.add(str);
+                frequencyStringMap.put(frequencyString, strList);
+            }
+        }
+
+        return new ArrayList<>(frequencyStringMap.values());
+    }
+
+    // Helper method to generate frequency signature
+    private String getFrequency(String str) {
+        int[] freq = new int[26];
+        for (char c : str.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        StringBuilder frequencyString = new StringBuilder();
+        char c = 'a';
+        for (int i : freq) {
+            frequencyString.append(c);
+            frequencyString.append(i);
+            c++;
+        }
+        return frequencyString.toString();
+    }
+
+    
+}
