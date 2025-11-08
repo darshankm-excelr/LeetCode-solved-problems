946001d377c06cd9837008ef1495c22010b54153
Darshan  K M <157715317+darshankm-excelr@users.noreply.github.com>
2025-11-08 20:20:31 +0530

    Time: 1 ms (41.55%) | Memory: 42.6 MB (5.07%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSegments(String s) {
+        s=s.trim();
+        if(s.isEmpty()){
+            return 0;
+
+        }
+        String [] s1=s.split("\\s+");
+        return s1.length;
+
+    }
+}
